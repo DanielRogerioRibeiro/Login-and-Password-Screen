bddc347cd5dcd61a1186fe63debf31aec7b3f614
DanielRogerioRibeiro <danielrogerioribeiro.dev@gmail.com>
2023-03-29 19:41:39 -0300

    First Commit

---
+++ Cadastro/login.py
@@ -0,0 +1,21 @@
+# Tela de Login e Senha
+from tkinter import *
+
+
+# Janela de login
+window = Tk()
+window.title ("AI QUE LINDO - LOGIN")
+window.geometry ('600x350') 
+window.iconphoto (False, PhotoImage(file="CADASTRO/imagens/aiquelindo.jpg"))
+window.resizable (width=False,height=False)
+
+#img = PhotoImage (file="imagens/aiquelindo.jpg")
+
+#Label.logo = Label(window, image=img, anchor='center')
+
+
+
+
+
+
+window.mainloop()
