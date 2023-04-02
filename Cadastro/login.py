0be0071dfb6539cb538b01e31170d00bda5a8474
DanielRogerioRibeiro <danielrogerioribeiro.dev@gmail.com>
2023-04-02 01:40:45 -0300

    window designer

--- Cadastro/login.py
+++ Cadastro/login.py
@@ -9,20 +9,22 @@ import customtkinter
 
 #Colors
 
-cor1 = "white"
-cor2 = "#bfb911" #gold
+cor1 = "#FFFFFF"
+cor2 = "#cebd7f" #gold
 cor3 = "black"
+cor4 = "#1f1f1e" #transparente
+cor5 = "#262626"
 
 customtkinter.set_appearance_mode("dark")
 #customtkinter.set_default_color_theme("dark-blue")
 
 
 # Janela de login
-window = customtkinter.CTk()
+window = Tk()
 window.title     ("AI QUE LINDO - LOGIN")
 window.geometry  ('487x383') 
-#window.iconphoto (PhotoImage(file="imagens/aiquelindo1.png"))
-window.resizable (width=True,height=True)
+window.iconphoto (False, PhotoImage(file="imagens/aiquelindo.png"))
+window.resizable (width=False,height=False)
 
 
 img = PhotoImage (file="imagens/aiquelindo-487-383.png")
@@ -50,21 +52,21 @@ def verificar_senha():
 
 
 #Configurando Email/Login
-label_name = customtkinter.CTkLabel(window, width=20, height=10, text="Name: *", anchor=NW, font=('Ivy', 12))
-label_name.place(x=50, y=80)
-e_name = customtkinter.CTkEntry(window, placeholder_text="Entry your Name", width=250, height=30, show="", justify='left', font=('', 10), border_width=0, corner_radius=10)
+label_name = Label(window, width=5, height=2, text="Name: *", fg=cor1, font=('Ivy', 9), anchor=NW, background=cor4)
+label_name.place(x=50, y=75)
+e_name = customtkinter.CTkEntry(window, placeholder_text="Entry your Name", width=250, height=30, show="", justify='left', font=('', 10), corner_radius=10, bg_color=cor4)
 e_name.place(x=50, y=100)
 
 #Configurando Password
-label_pass = customtkinter.CTkLabel(window, width=20, height=10, text="Password *", anchor=NW, font=('Ivy', 12))
+label_pass = Label(window, width=15, height=2, text="Password: *", fg=cor1, font=('Ivy', 9), anchor=NW, background=cor4)
 label_pass.place(x=50, y=155)
-e_pass = customtkinter.CTkEntry(window, placeholder_text="Entry your Password" , width=250, height=30, show="*", justify='left', font=('', 10), border_width=0, corner_radius=10)
+e_pass = customtkinter.CTkEntry(window, placeholder_text="Entry your Password" , width=250, height=30, show="*", justify='left', font=('', 10), corner_radius=10, bg_color=cor4)
 e_pass.place(x=50, y=175)
 
-checkbox = customtkinter.CTkCheckBox(window, checkbox_height=13, checkbox_width=13 , text="Lembrar Login")
-checkbox.place(x=250, y=230, anchor=CENTER)
+checkbox = customtkinter.CTkCheckBox(window, text="Lembrar Login", checkbox_width=15, checkbox_height=15, bg_color=cor4)
+checkbox.place(x=100, y=230, anchor=CENTER)
 
-b_confirmar = customtkinter.CTkButton(window, command=verificar_senha, text="ENTRAR", width=150, height=32, border_width=0, corner_radius=10)
+b_confirmar = customtkinter.CTkButton(window, command=verificar_senha, text="ENTRAR", width=150, height=32, border_width=0, corner_radius=10, bg_color='blue')
 b_confirmar.place(x=250, y=270, anchor=CENTER)
 
 
