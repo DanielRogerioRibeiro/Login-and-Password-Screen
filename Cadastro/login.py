1b2bb6cfa480cf7acdfbabad21f055ff17d0ab4c
DanielRogerioRibeiro <danielrogerioribeiro.dev@gmail.com>
2023-04-01 22:29:16 -0300

    Insert the checkbox Login

--- Cadastro/login.py
+++ Cadastro/login.py
@@ -6,36 +6,36 @@ import customtkinter
 
 
 
+
 #Colors
 
 cor1 = "white"
 cor2 = "#bfb911" #gold
 cor3 = "black"
 
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
 
 # Janela de login
 window = customtkinter.CTk()
 window.title     ("AI QUE LINDO - LOGIN")
-window.geometry  ('600x350') 
-#window.iconphoto (PhotoImage(file="imagens/aiquelindo1.ico"))
+window.geometry  ('487x383') 
+#window.iconphoto (PhotoImage(file="imagens/aiquelindo1.png"))
 window.resizable (width=True,height=True)
 
-img = PhotoImage (file="imagens/aiquelindo.png")
 
-Label.logo = Label(window, image=img, width=600, height=350)
+img = PhotoImage (file="imagens/aiquelindo-487-383.png")
+
+Label.logo = Label(window, image=img, width=487, height=383)
 Label.logo.place(x=0, y=0)
-
-
-#Configurando o frame cima
-label_login = Label(window, text="LOGIN", anchor=NE, font=('Ivy 25 bold'), fg=cor2, bg=None)
-label_login.place(x=5, y=5)
 
 
 #Credenciais
 credenciais = ["joao", "123456"]
 
 def verificar_senha():
-    nome = e_nome.get()
+    nome = e_name.get()
     senha = e_pass.get()
 
     if nome == "admin" and senha == "admin":
@@ -49,21 +49,23 @@ def verificar_senha():
 
 
 
-#Configurando o frame baixo
-label_nome = Label(window, text="NOME *", anchor=NW, font=('Ivy 10'), fg=cor3)
-label_nome.place(x=50, y=60)
-e_nome = Entry(window, width=25, justify='left', font=('', 15), highlightthickness=1, relief=SOLID)
-e_nome.place(x=50, y=100)
+#Configurando Email/Login
+label_name = customtkinter.CTkLabel(window, width=20, height=10, text="Name or E-mail *", anchor=NW, font=('Ivy', 10))
+label_name.place(x=50, y=60)
+e_name = Entry(window, width=25, justify='left', font=('', 15), highlightthickness=1, relief=SOLID)
+e_name.place(x=50, y=100)
 
-
-label_pass = Label(window, text="PASSWORD *", anchor=NW, font=('Ivy 10'), fg=cor3)
+#Configurando Password
+label_pass = customtkinter.CTkLabel(window, width=20, height=10, text="Password *", anchor=NW, font=('Ivy', 10))
 label_pass.place(x=50, y=150)
-e_pass = Entry(window, width=25, show="*", justify='left', font=('', 15), highlightthickness=1, relief=SOLID)
-e_pass.place(x=50, y=190)
+e_pass = customtkinter.CTkEntry(window, width=250, height=30, show="*", justify='left', font=('', 15), border_width=0, corner_radius=10)
+e_pass.place(x=50, y=175)
 
+checkbox = customtkinter.CTkCheckBox(window, checkbox_height=13, checkbox_width=13 , text="Lembrar Login")
+checkbox.place(x=250, y=230, anchor=CENTER)
 
-b_confirmar = Button(window, command=verificar_senha, text="ENTRAR", width=39, height=2, font=('Ivy 8 bold'), fg=cor3, relief=RAISED, overrelief=RIDGE)
-b_confirmar.place(x=160, y=300)
+b_confirmar = customtkinter.CTkButton(window, command=verificar_senha, text="ENTRAR", width=150, height=32, border_width=0, corner_radius=10)
+b_confirmar.place(x=250, y=270, anchor=CENTER)
 
 
 
